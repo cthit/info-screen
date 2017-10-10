1e795811b67e9f2b73473effd56bae3c5d872d82
Johan Lindskogen <johan.lindskogen@gmail.com>
2017-10-10 21:39:31 +0200

    lunch support

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import feedparser
 from inflection import parameterize
 from mako.template import Template
+import requests
+import json
 
 def parse_alt_xkcd(entry):
 	parsed_description = feedparser.parse(entry['description'])
@@ -21,22 +23,46 @@ feeds = [{
 	'count': 3
 }]
 
-for feed_data in feeds:
-	feed = feedparser.parse(feed_data.get('url'))
-	items = []
-	filename = parameterize(feed_data.get('name')) + ".html"
-	f = open(filename, 'w')
+def out_filename(name):
+	return "%s.html" % (name)
 
-	for i in range(0, feed_data.get('count')):
-		entry = feed['entries'][i]
-		item = {
-			'title': entry['title'],
-			'description': feed_data.get('parse_description', parse_description)(entry),
-			'alt_text': feed_data.get('parse_alt', lambda x: '')(entry)
-		}
+def template_path(template):
+	return "templates/%s.html" % (template)
 
-		items.append(item)
+def fetch_feeds():
+	for feed_data in feeds:
+		feed = feedparser.parse(feed_data.get('url'))
+		items = []
+		filename = out_filename(parameterize(feed_data.get('name')))
+		f = open(filename, 'w')
 
-	template = Template(filename="templates/%s.html" % (feed_data.get('layout')))
-	f.write(template.render(items=items))
+		for i in range(0, feed_data.get('count')):
+			entry = feed['entries'][i]
+			item = {
+				'title': entry['title'],
+				'description': feed_data.get('parse_description', parse_description)(entry),
+				'alt_text': feed_data.get('parse_alt', lambda x: '')(entry)
+			}
+
+			items.append(item)
+
+		template = Template(filename=template_path(feed_data.get('layout')))
+		f.write(template.render(items=items))
+		f.close()
+
+
+def fetch_lunch():
+	lunch_url = "https://lunch.chalmers.it/"
+
+	r = requests.get(lunch_url)
+	r.raise_for_status()
+
+
+	f = open(out_filename("lunch"), 'w')
+	template = Template(filename=template_path("lunch"))
+	restaurants = json.loads(r.text)
+	f.write(template.render(restaurants=restaurants))
 	f.close()
+
+
+fetch_lunch()
